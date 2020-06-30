d7d0b0eea4aa9a492c0eaaf5239404a642ed52b2
Gharam Alsaedi <gbalsaedi@ucdavis.edu>
2020-06-30 14:44:05 -0700

    Updates

---
+++ precache-manifest.6afddea60e5eb1a8db3b357ec11c6b36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f621d4c8555cb8411cfb809f5c020c78",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "367f42a4ce0d9afb38d0",
+    "url": "/portfolio-site/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "000dcbc94d9cb4d62514",
+    "url": "/portfolio-site/static/js/2.c2ac23dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-site/static/js/2.c2ac23dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "367f42a4ce0d9afb38d0",
+    "url": "/portfolio-site/static/js/main.6e958171.chunk.js"
+  },
+  {
+    "revision": "a2a773adb23ff47de08a",
+    "url": "/portfolio-site/static/js/runtime-main.2d95e7a6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
